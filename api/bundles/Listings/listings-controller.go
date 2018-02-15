ca3edf424f9efe9126d2881372052c55ad4c39c7
Jason Wilson <jason@wilsons.io>
2018-02-15 13:16:21 -0500

    Adding pseudocode for Listings API bundle

---
+++ api/bundles/Listings/listings-controller.go
@@ -0,0 +1,38 @@
+/* Controller package for listings */
+
+// Import Http
+// Import Common bundle
+
+// Set up ListingsController struct to include Common.JSON
+
+// Define handler to return all Listings
+//		Params: realm, offset, limit
+//		Query database
+//		Return JSON
+
+// Define handler to return Listing matching ID
+//		Params: id
+//		Query database
+//		Return as JSON
+
+// Define handler to return seller for a given listing ID
+//		Params: id
+//		Query database
+//		Return as JSON
+
+// Define handler to create a new listing
+//		Params: listingObject
+//		Validate data
+//		Insert to database
+//		Return listing from database response
+
+// Define handler to update a listing
+//		Params: listingObject
+//		Validate data
+//		Insert to database
+//		Return listing from database response
+
+// Define handler to delete a new listing
+//		Params: listingId
+//		Drop from database
+//		Return database response
