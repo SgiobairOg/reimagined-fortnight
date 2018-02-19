2f29248f8ff68dec1c471713042ab99fbefee7d2
Jason Wilson <jason@wilsons.io>
2018-02-19 14:33:23 -0500

    Removing API bundles

--- api/bundles/Listings/listings-model.go
+++
@@ -1,3 +0,0 @@
-// Define the structure for Listing model
-
-// Provide constructor for a new Listing object
--- api/bundles/Sellers/sellers-model.go
+++
@@ -1,3 +0,0 @@
-// Define the structure for Sellers model
-
-// Provide constructor for a new Seller object
